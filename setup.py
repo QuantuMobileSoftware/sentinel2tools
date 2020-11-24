e8e1f9d4821271f93eee5e63c77bf9256665cd2f
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-24 15:03:29 +0200

    13723: upd setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='sentinel2tools',
     version=__version__,
     author=__author__,
-    packages=find_packages(include=['sentinel2tools', ]),
+    packages=find_packages('sentinel2download'),
     setup_requires=install_requires,
     install_requires=install_requires,
     package_data={'sentinel2download': ['grid/*', '../requirements-dev.txt']},
