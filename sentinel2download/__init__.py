6bf61770084969c69bab77d2d1858b41f3b192f7
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-24 16:21:35 +0200

    13723: upd init, setup

--- sentinel2download/__init__.py
+++ sentinel2download/__init__.py
@@ -1,5 +1,7 @@
 __version__ = '1.0'
 __author__ = 'Quantumobile'
 
+from sentinel2download.downloader import Sentinel2Downloader
+from sentinel2download.overlap import Sentinel2Overlap
 # from .overlap import Sentinel2Overlap
 # from .downloader import Sentinel2Downloader
--- setup.py
+++ setup.py
@@ -9,13 +9,9 @@ setup(
     name='sentinel2tools',
     version=__version__,
     author=__author__,
-    packages=find_packages(),
-    # setup_requires=install_requires,
-    install_requires=['google-cloud-storage==1.32.0',
-                    'geopandas==0.8.1',
-                    'Rtree==0.9.4', ],
-    # package_data={'sentinel2download': ['grid/*', '../requirements-dev.txt']},
+    packages=find_packages('sentinel2download'),
+    install_requires=install_requires,
+    package_data={'sentinel2download': ['grid/*', ]},
     python_requires='>=3.7',
-    # dependency_links=['git+ssh://git@github.com/QuantumobileSoftware/sentinel2tools.git@13723_vy_load_sentinel_setup']
 
 )
