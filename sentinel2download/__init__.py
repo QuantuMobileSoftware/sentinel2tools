bd0ad1dc112413c7374f8b5de44f81cb3da96e14
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-24 18:10:32 +0200

    13723: update init

--- sentinel2download/__init__.py
+++
@@ -1,6 +0,0 @@
-
-
-from sentinel2download.downloader import Sentinel2Downloader
-from sentinel2download.overlap import Sentinel2Overlap
-# from .overlap import Sentinel2Overlap
-# from .downloader import Sentinel2Downloader
