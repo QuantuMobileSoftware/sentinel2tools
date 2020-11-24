7dedf3af9b76369a3613763f8a9677b5c7afb7b2
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-24 12:38:31 +0200

    13723: add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from sentinel2download import __version__, __author__
+from setuptools import setup, find_packages
+
+install_requires = ['google-cloud-storage==1.32.0',
+                    'geopandas==0.8.1',
+                    'Rtree==0.9.4', ]
+
+setup(
+    name='sentinel2tools',
+    version=__version__,
+    author=__author__,
+    packages=find_packages(include=['sentinel2download', ]),
+    install_requires=install_requires,
+    package_data={'sentinel2download': ['grid/*', '../requirements-dev.txt']},
+    python_requires='>=3.6',
+
+)
