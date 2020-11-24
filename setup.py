664ba9b1b47a5f840be8e6ab57ce0901f3d50f54
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-24 15:17:54 +0200

    13723: upd setup

--- setup.py
+++ setup.py
@@ -9,10 +9,12 @@ setup(
     name='sentinel2tools',
     version=__version__,
     author=__author__,
-    packages=find_packages('*'),
-    setup_requires=install_requires,
-    install_requires=install_requires,
-    package_data={'sentinel2download': ['grid/*', '../requirements-dev.txt']},
+    packages=find_packages(),
+    # setup_requires=install_requires,
+    install_requires=['google-cloud-storage==1.32.0',
+                    'geopandas==0.8.1',
+                    'Rtree==0.9.4', ],
+    # package_data={'sentinel2download': ['grid/*', '../requirements-dev.txt']},
     python_requires='>=3.7',
 
 )
