e57f7582fe253cf52ba84a2ad209c26a3bdfafe4
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-25 11:04:54 +0200

    13723: upd main-dev

--- main-dev.py
+++ main-dev.py
@@ -3,10 +3,11 @@ from sentinel2download.overlap import Sentinel2Overlap
 
 if __name__ == '__main__':
     verbose = True
-    aoi_path = "../test_geojson/Kharkiv.geojson"
+    aoi_path = "../test_geojson/osnova_lake.geojson"
 
     overlap = Sentinel2Overlap(aoi_path, verbose=verbose)
     tiles = overlap.overlap()
+    # tiles = tiles[:1]
 
     print(f"Overlapped tiles: {tiles}")
 
@@ -14,18 +15,18 @@ if __name__ == '__main__':
 
     loader = Sentinel2Downloader(api_key, verbose=verbose)
 
-    product_type = 'L1C'  # or L2A
-    start_date = "2020-07-01"
-    end_date = "2020-07-03"
+    product_type = 'L2A'  # or L1C
+    start_date = "2020-10-01"
+    # end_date = "2020-07-03"
     output_dir = '../sentinel2imagery'
-    cores = 5
-    BANDS = {'TCI', }
-    CONSTRAINTS = {'NODATA_PIXEL_PERCENTAGE': 100.0, 'CLOUDY_PIXEL_PERCENTAGE': 100.0, }
+    cores = 3
+    BANDS = {'TCI', 'B04'}
+    CONSTRAINTS = {'NODATA_PIXEL_PERCENTAGE': 15.0, 'CLOUDY_PIXEL_PERCENTAGE': 10.0, }
 
     loaded = loader.download(product_type,
                              tiles,
                              start_date=start_date,
-                             end_date=end_date,
+                             # end_date=end_date,
                              output_dir=output_dir,
                              cores=cores,
                              bands=BANDS,
